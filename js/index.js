7a32c8a057139ad67ac1ff9f2e1e05d0af537a32
Anna Bezrukova <annelia_55@mail.ru>
2016-12-11 05:18:13 +0300

    initial

---
+++ js/index.js
@@ -0,0 +1,5 @@
+/* Текст в поле input не обрезается */
+var input = document.querySelectorAll('input');
+for(i=0; i<input.length; i++){
+    input[i].setAttribute('size',input[i].getAttribute('placeholder').length);
+}
